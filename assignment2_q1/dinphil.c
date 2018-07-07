731ac9293d0d4446eec2dae126f980785ef6148c
Eton <etonk@sfu.ca>
2018-07-07 12:40:54 -0700

    Done

--- assignment2_q1/dinphil.c
+++ assignment2_q1/dinphil.c
@@ -10,15 +10,16 @@ File: dinphil.c
 #include<stdio.h>
 #include<semaphore.h>
 #include<pthread.h>
+#include<signal.h>
  
 sem_t mphil[5]; /* Total of 5 Philosophers */ 
 int state[5] = {};
-pthread_cond_t eat_cv[5];
-pthread_cond_t think_cv[5];
-pthread_mutex_t mut_state;
-pthread_mutex_t mp[5];
+pthread_cond_t eat_cv[5] = PTHREAD_COND_INITIALIZER;
+pthread_cond_t think_cv[5] = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t mut_state = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mp[5] = PTHREAD_MUTEX_INITIALIZER;
 
-void test(int num)
+void isHungry(int num)
 {
     if (num == 0)
     {
@@ -38,16 +39,16 @@ void test(int num)
     }
 }
 
-void phil(int num)
+void* phil(int num)
 {
-    while(true)
+    while(1)
     {
         /* Thinking */
         pthread_cond_wait(&think_cv[num], &mp[num]);
         /* Grab fork */
         pthread_mutex_lock(&mut_state);
         state[num] = 2;
-        test(num);
+        isHungry(num);
         pthread_mutex_unlock(&mut_state);
         sem_wait(&mphil[num]);
         
@@ -58,16 +59,16 @@ void phil(int num)
         state[num] = 0;
         if (num == 0)
         {
-            test(num+1);
+            isHungry(num+1);
         }
         else if (num == 4)
         {
-            test(num-1);
+            isHungry(num-1);
         }
         else
         {
-            test(num+1);
-            test(num-1);
+            isHungry(num+1);
+            isHungry(num-1);
         }
         pthread_mutex_unlock(&mut_state);
     }
@@ -76,55 +77,50 @@ void phil(int num)
 int main()
 {
     int i, input_num = 0;
-//    int input_num = 0;
     char input_char = ' ';
-    
+    char c;
     pthread_t tid[5];
-    pthread_mutex_t(&mut_state, NULL);
     
-    for(i=0;i<5;i++){
+    for(i=0;i<5;++i){
         sem_init(&mphil[i],0,0);
-        pthread_cond_init(&eat_cv[i], NULL);
-        pthread_cond_init(&think_cv[i], NULL);
-        pthread_mutex_init(&mp[i], NULL);
-        pthread_create(&tid[i],NULL,phil, i);
+        pthread_create(&tid[i],NULL,(void *)phil, (void *) i);
     }
-      
-    while (true)
+    
+    while (1)
     {
-        scanf("%c", &input_char);
+        scanf(" %c", &input_char);
         
         if(input_char == '!')
         {
             /* Exit Program */ 
-            for (i = 0; i < 5; i++)
-            {
-                /* Close all threads */
-                pthread_join(&tid[i], NULL);
-                sem_destory(&mphil[i]);
+            for (i = 0; i < 5; ++i)
+            {            
+		/* Close all threads */
+                pthread_kill(tid[i],0);
+                sem_close(&mphil[i]);
                 pthread_mutex_destroy(&mp[i]);
                 pthread_cond_destroy(&eat_cv[i]);
                 pthread_cond_destroy(&think_cv[i]);
             }
             break;
         }
-        else if (input_char == 't' || input_char == 'T')
+        else if (input_char == 'T')
         {
             /* Request Philosopher to Think */
             scanf("%d", &input_num);
             pthread_cond_signal(&eat_cv[input_num]);
         }
-        else if (input_char == 'e' || input_char == 'E')
+        else if (input_char == 'E')
         {
             /* Request Philosopher to Eat */
             scanf("%d", &input_num);
             pthread_cond_signal(&think_cv[input_num]);
         }
-        else if (input_char == 'p' || input_char == 'P')
+        else if (input_char == 'P')
         {
             /* Print states of philosophers 
                Note: when state is 2, it is in hungry(2) which is same as thinking (0) */
-            for(i = 0; i < 4; i++)
+            for(i = 0; i < 4; ++i)
             {
                 printf("%d ", (state[i] == 2? 0:state[i]));
             }
@@ -132,9 +128,7 @@ int main()
         }
         else
         {
-            printf("Error: unknown input \n");
-            printf("Only allow T,E,P, and ! \n");
         }
     }    
-    return NULL;
+    return 0;
 }
