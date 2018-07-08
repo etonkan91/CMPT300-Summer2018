c6a73c08af93be6c195474d5acbb59b323bc4fbd
Eton <etonk@sfu.ca>
2018-07-08 12:44:50 -0700

    Added compile instruction

--- assignment2/assignment2_q1/dinphil.c
+++ assignment2/assignment2_q1/dinphil.c
@@ -4,9 +4,10 @@ Author: Hei Tung Kan
 Student ID: 301235768
 Course: CMPT300
 Date: July 8, 2018
-Assignment 2 Question 1
 File: dinphil.c
+Compile Instruction: gcc dinphil.c -o dinphil -lpthread
 
+Assignment 2 Question 1
 Example deadlock scenario:
 Resource hierarchy solution for dining philosopher will not generate deadlocks because the lower number philosopher must wait for the higher number philosopher to finish eating before locking any resources. Seem the highest philosopher will always be able to eat (it have its own fork and lower number philosopher must wait for higher number philosopher to finish eating). Deadlocks where two philosophers are waiting for each other's resource will not happen. Therefore, there are no specific input and expected output for TA to verify as Deadlock will never occur.
 */
